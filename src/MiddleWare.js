2795fb2d2a97a53254e4cafd8419e1219df47aed
Jeewon Park <redbeanmouse@gmail.com>
2023-01-17 12:14:35 +0900

    실제 GET을 처리할 클래스

---
+++ src/MiddleWare.js
@@ -0,0 +1,38 @@
+const { response } = require('express');
+const request = require('request')
+const requestPromise = require('request-promise')
+const url = require('url')
+
+
+class MiddleWare
+{
+    constructor() {
+        this.baseURL = '';
+        this.port = '';
+        this.query = '/bot';
+    }
+
+    BaseURLs(req, res) {
+      res.send("hi");
+    }
+
+
+    async RequestAnswer(reqText) {
+        
+        //var result = await requestPromise(options);
+        request(options, function (error, response) {
+            if (error) throw new Error(error);
+            console.log(response.body);
+            return response.body;
+          });
+    }   
+
+    AskToServer(req, res) {
+        var text = req.query.q;
+        res.send(result.toString())
+
+    }
+}
+
+
+module.exports = MiddleWare
