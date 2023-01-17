8fe5bfe9fdd579177c8012f169aeacddb15a9dc5
Jeewon Park <redbeanmouse@gmail.com>
2023-01-17 12:13:45 +0900

    GET 용 API 확장 클래스

---
+++ src/AppRouter.js
@@ -0,0 +1,20 @@
+const AppBase = require('../JWPak/expressApp');
+const MiddleWareFuntions = require('./MiddleWare.js')
+
+class appExt extends AppBase {
+    constructor_Internal() {
+        this.mw = new MiddleWareFuntions();
+        this.BuildAPIs()
+        super.StartServer();
+        console.log(this.port);
+    }
+
+    BuildAPIs() {
+        this.app.get('/', this.mw.BaseURLs);
+        //this.app.get('/api', this.nlp.AskToServer);
+        //this.app.get('/:q', this.nlp.AskToServer);
+        //this.app.get('/d', this.mw.CheckTime);
+    }
+}
+
+module.exports = appExt;
